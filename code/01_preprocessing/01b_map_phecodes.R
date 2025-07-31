94f45f39f5267bed65821823135f9eb1260ced95
Janne Auning Hansen <79196745+janneah@users.noreply.github.com>
2025-07-31 11:21:11 +0200

    updated mapping

--- code/01_preprocessing/01b_map_phecodes.R
+++ code/01_preprocessing/01b_map_phecodes.R
@@ -1,138 +1,140 @@
-#' Script for translating ICD codes into phecodes.
-#'
-#' @author Janne AH
-#' @date 01-03-2024
-#' 
-#' @description In the code, any ICD8 codes in the cohort of interest are first translated to ICD10, and then all ICD10 codes are translated into phecodes
-#' 
+#' @title ICD8 → ICD10 → Phecode Mapping Pipeline
+#' @description Loads diagnosis data, translates ICD-8 codes to ICD-10, then maps to phecodes.
+#' @details This script is designed for Danish registry data with simulated twin/sibling structure.
 
+# ------------------------------------------------------------------
+# Setup ------------------------------------------------------------
+# ------------------------------------------------------------------
 
-# Libs -----
+# Only load 'here', use :: for all other package functions
+library(here)
 
-library(dplyr)
-library(data.table)
-library(tidyr)
-library(stringr)
+# ------------------------------------------------------------------
+# Load data --------------------------------------------------------
+# ------------------------------------------------------------------
 
-# Loading in data ---------------------------------------------------------
-lpr <- readRDS("simulated_twin_lpr.rds")) 
+lpr  <- readRDS(here::here("data", "simulated_twin_lpr.rds"))
+stam <- readRDS(here::here("data", "stam_data.rds"))
 
+# ------------------------------------------------------------------
+# Load helpers and mappings ----------------------------------------
+# ------------------------------------------------------------------
 
-# Loading in aux code and maps ----------------------------------------------
-source("code/helper_functions/from_icd_to_phecodes.R")
+source(here::here("code", "helper_functions", "from_icd_to_phecodes.R"))
 
-# Map to translate ICD8 to ICD10
-icd8_map <- fread("data/maps/mapICD8ToICD10.txt", sep = "\t") %>%
-  # Doing some basic reformatting of some of the cols
-  mutate(START_DATE = as.Date(as.character(START_DATE), format = "%Y%m%d"),
-         END_DATE = as.Date(as.character(END_DATE), format = "%Y%m%d"),
-         ICD8_CODE = as.character(ICD8_CODE),
-         ICD10_CODE = ifelse(ICD10_CODE == "", NA, ICD10_CODE)) %>% 
-  filter(!is.na(ICD10_CODE))                                # Some ICD8s are in the map but without any ICD10, these are removed
+# ICD8 → ICD10 mapping
+icd8_map <- data.table::fread(here::here("data", "maps", "mapICD8ToICD10.txt"), sep = "\t") |>
+  dplyr::mutate(
+    START_DATE  = as.Date(as.character(START_DATE), format = "%Y%m%d"),
+    END_DATE    = as.Date(as.character(END_DATE), format = "%Y%m%d"),
+    ICD8_CODE   = as.character(ICD8_CODE),
+    ICD10_CODE  = dplyr::na_if(ICD10_CODE, "")
+  ) |>
+  dplyr::filter(!is.na(ICD10_CODE))
 
-# Phecode map and metainfo
-load("data/maps/pheinfo.rda")
-load("data/maps/sex_restriction.RData")
-phecode_map <- readRDS("data/maps/phecode_icd10.rds")      # This .rds version is slightly edited from the original one
+# Phecode map and phenotype info
+load(here::here("data", "maps", "pheinfo.rda"))
+load(here::here("data", "maps", "sex_restriction.RData"))
+phecode_map <- readRDS(here::here("data", "maps", "phecode_icd10.rds"))
 
-# Pivot longer ----------------------------------------------------------------
-# Going from having three columns with diagnoses to one. Removing NAs and duplicate diagnoses
+# ------------------------------------------------------------------
+# Long format diagnosis list ---------------------------------------
+# ------------------------------------------------------------------
+
 icd_col <- "icd"
 
-long_lpr <- pivot_longer_lpr(lpr,
-                             new_col = icd_col,
-                             cols = c("c_diag", "c_adiag", "c_tildiag"))
+long_lpr <- pivot_longer_lpr(
+  lpr,
+  new_col = icd_col,
+  cols = c("c_diag", "c_adiag", "c_tildiag")
+)
 
-head(long_lpr)
+# ------------------------------------------------------------------
+# ICD8 → ICD10 -----------------------------------------------------
+# ------------------------------------------------------------------
 
-# ICD8 to ICD10 ---------------------------------------------------------------
 icd10_col <- "icd10"
-# debugonce(translate_icd8) # For debugging
-lpr_icd10 <- translate_icd8(lpr = long_lpr, 
-                            col = icd_col,
-                            new_col = icd10_col,
-                            map = icd8_map)
 
-head(lpr_icd10)
+lpr_icd10 <- translate_icd8(
+  lpr      = long_lpr,
+  col      = icd_col,
+  new_col  = icd10_col,
+  map      = icd8_map
+)
 
-# ICD10 to phecodes -----------------------------------------------------------
+# ------------------------------------------------------------------
+# Reformat ICD10 ---------------------------------------------------
+# ------------------------------------------------------------------
 
-# Removing trailing letters in the ICD10s (mainly a Danish convention)
-# E.g. R10.2C becomes R10.2
+lpr_reformatted_icd10 <- format_icd10(
+  lpr = lpr_icd10,
+  col = icd10_col
+)
 
-# Reformatting the ICD10s - inserting . and removing trailing letters
+# ------------------------------------------------------------------
+# ICD10 → Phecodes -------------------------------------------------
+# ------------------------------------------------------------------
 
-##### Consider moving this part to before ICD8-ICD10 as there is a mixture of ICD10 codes with different format (see Betinas comment)
-##### Consider removing "." from map rather than inserting it in dataset
-
-lpr_reformatted_icd10 <- format_icd10(lpr = lpr_icd10,
-                                      col = icd10_col)
-
-head(lpr_reformatted_icd10)
-
-# Translating the formatted ICD codes to phecodes
-# This is done in the following steps:
-
-# 1. First naive mapping of ICD10 codes to phecodes:
 phecode_col <- "phecode"
-# debugonce(naive_mapping)
-lpr_phecodes <- naive_mapping(lpr = lpr_reformatted_icd10, 
-                              icd10_col = icd10_col,
-                              phecode_col = phecode_col,
-                              map = phecode_map)
 
-# 2. Some of the ICD10 do not map directly, however. Possibly due to some specific ICD10 codes
-# being used mainly in the Danish system and not in the American. Such codes (as e.g. many of
-# the .9 codes, are simply mapped to a parental code, e.g., F30.9 being mapped to F30)
+# Step 1: Naive mapping
+lpr_phecodes <- naive_mapping(
+  lpr          = lpr_reformatted_icd10,
+  icd10_col    = icd10_col,
+  phecode_col  = phecode_col,
+  map          = phecode_map
+)
 
-lpr_phecodes <- broader_mapping(lpr = lpr_phecodes,
-                                icd10_col = icd10_col,
-                                phecode_col = phecode_col,
-                                map = phecode_map)
+# Step 2: Broader mapping (e.g. F30.9 → F30)
+lpr_phecodes <- broader_mapping(
+  lpr          = lpr_phecodes,
+  icd10_col    = icd10_col,
+  phecode_col  = phecode_col,
+  map          = phecode_map
+)
 
-head(lpr_phecodes)
+# Step 3: Add all parental phecodes (e.g. 803.12 → 803.1 and 803)
+lpr_parental_phecodes <- parental_phecodes(
+  lpr          = lpr_phecodes,
+  phecode_col  = phecode_col,
+  new_col      = "all_phecodes",
+  map          = phecode_map
+)
 
+# ------------------------------------------------------------------
+# Build phenotype matrix -------------------------------------------
+# ------------------------------------------------------------------
 
-# 3. Now, all diagnoses should be counted in all levels of the phecode, so if an individual 
-# has the phecode 803.12, it should also be mapped to 803.1 and 803 
-lpr_parental_phecodes <- parental_phecodes(lpr = lpr_phecodes,
-                                           phecode_col = phecode_col,
-                                           new_col = "all_phecodes",
-                                           map = phecode_map)
-  
-head(lpr_parental_phecodes)
+# Filter sex-restricted phenotypes
+sex_restriction <- dplyr::left_join(
+  sex_restriction,
+  pheinfo[, c("phecode", "description")],
+  by = "phecode"
+) |>
+  dplyr::filter(male_only == TRUE | female_only == TRUE)
 
-# Now creating the df with twin/sibling info as well as case/control status for all phecodes with at least 400 cases (0.5%) -----
+# Define phenotypes with ≥ 400 cases
+pheno_400 <- lpr_parental_phecodes |>
+  dplyr::group_by(Phenotype, phecode) |>
+  dplyr::summarise(n = dplyr::n(), .groups = "drop") |>
+  dplyr::arrange(dplyr::desc(n)) |>
+  dplyr::filter(!Phenotype %in% sex_restriction$description) |>
+  dplyr::filter(n >= 400)
 
-# Filtering
-sex_restriction <- sex_restriction %>% 
-  left_join(pheinfo[, c("phecode", "description")], by = "phecode") %>% 
-  filter(male_only == TRUE | female_only == TRUE)
+# ------------------------------------------------------------------
+# Case-control and filtering ---------------------------------------
+# ------------------------------------------------------------------
 
+dx_status <- case_control_status(
+  stam       = stam,
+  lpr        = lpr_parental_phecodes,
+  phenotypes = pheno_400
+)
 
-
-# Making case control matrix ------------------------------------------------------------------------------
-if(pair == "twins") {
-  stam <- readRDS(paste0("steps/stam_lpr/linked_twins_", birth_cohort, ".rds"))
-} else {
-  stam <- readRDS(paste0("steps/stam_lpr/all_linked_sibs_", birth_cohort, ".rds"))
-}
-
-# Finding phenotypes with minimally 400 cases
-pheno_400 <- lpr_parental_phecodes %>% 
-  group_by(Phenotype, phecode) %>% 
-  summarise(n = n()) %>%
-  arrange(desc(n)) %>% 
-  filter(!Phenotype %in% sex_restriction$description) %>% # Sex restriction def from PheWas
-  filter(n >= 400)  
-
-# Making case control status df
-# debugonce(case_control_status)
-dx_status <- case_control_status(stam, lpr_parental_phecodes, pheno_400) # Some issue with 1955 sibs, CHECK that inds are counted correctly
-
-# Filtering away phenotypes with imbalanced sex ratios
 dx_status_flt <- remove_imbalanced_pheno(dx_status)
 
+# ------------------------------------------------------------------
+# Save results -----------------------------------------------------
+# ------------------------------------------------------------------
 
-saveRDS(dx_status_flt, paste0("steps/pheno/", pair, "_", birth_cohort, "_phecode_status_min400.rds"))
-
+saveRDS(dx_status_flt, here::here("steps", "dx_status_min400.rds"))
